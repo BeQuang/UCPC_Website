0f7280dc7f0ff758f4d8cf8668aa60f973da9bc7
BEQUANG2409 <21522507@gm.uit.edu.vn>
2024-04-25 22:57:19 +0700

    Create the MainLayout branch

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,10 @@
+import classNames from 'classnames/bind';
+import styles from './Home.module.scss';
+
+const cx = classNames.bind(styles);
+
 function Home() {
-    return <h2>Home page</h2>;
+    return <h2 className={cx('wrapper')}>Home page</h2>;
 }
 
 export default Home;
