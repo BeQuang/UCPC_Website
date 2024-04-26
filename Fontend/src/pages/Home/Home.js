2cc75d8f056837feb6a157109abc6b8d3a3f91b1
xlinh2301 <nxlinh2301@gmail.com>
2024-04-27 00:41:14 +0700

    home page co ban

--- Fontend/src/pages/Home/Home.js
+++ Fontend/src/pages/Home/Home.js
@@ -1,10 +1,31 @@
+import React from 'react';
 import classNames from 'classnames/bind';
 import styles from './Home.module.scss';
 
 const cx = classNames.bind(styles);
 
 function Home() {
-    return <h2 className={cx('wrapper')}>Home page</h2>;
+  return (
+    <div>
+      <header className={cx('header')}>
+        Header content
+      </header>
+      <body className={cx('body')}>
+        <div className={cx('rectangle')}>
+          <div className={('content')}>
+            <div className={cx('title')}>
+              <h1>UIT COLLEGIATE</h1>
+              <h2>PROGRAMMING CONTEST</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam cursus varius quam in tincidunt. Vestibulum a ultricies risus. Aenean auctor sodales dolor non posuere. Vestibulum nec nulla posuere, sollicitudin ligula.</p>
+            </div>
+            <button className={cx('botton')}>Đăng ký tham gia</button>
+          </div>
+        </div>
+      </body>
+      <footer className={cx('footer')}>
+      </footer>
+    </div>
+  );
 }
 
 export default Home;
