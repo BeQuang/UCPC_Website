58600c2fe73df83c74d12c9b6b5ab0917060ccc1
xlinh2301 <nxlinh2301@gmail.com>
2024-04-28 02:05:59 +0700

    page 3 done

--- Fontend/src/pages/Home/Home.js
+++ Fontend/src/pages/Home/Home.js
@@ -47,10 +47,59 @@ function Home() {
         </div>
         <div className={cx('body2')}>
           <div className={cx('body2__content')}>
-            <img src="https://s3-alpha-sig.figma.com/img/2a82/9740/50cac91b6c99f1dd6b818fc2c799e3d5?Expires=1714953600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=CJmnMN~xeNaFztTd-o5LmJDV4Z8rQkVhhLvHO9qoAqfNOc1V7N6DhiGjJYBladto6Z2aSuOEKn3fC1~Pho5rdTidW2Kq6t4M4SoYL3MoMX9lZ7ocLSUJ4qJaOsXe~tGYUTVSGUFMwBR0V0DAP4vhXVwUenP67Ox-13jVE60YaRV0usEJGqUn4CA7Z2Ns0uBEIBmgRtxgvzT1CYQlZDp6yPcGURIbr7PikwhTz21SitAav0eKfQ5ARZgUhiscP7yyUfar8gN47lar3z--MpSmnNpZwZ-4ZdSBet4m7UwmoYUm8~YCfmN6~genc~I3iVBh9lpe-oNxbW-2ASGjLzSyaA__" alt="" className={cx('logo')} />
-            <h1>UCPC 2024</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <img src="https://s3-alpha-sig.figma.com/img/2a82/9740/50cac91b6c99f1dd6b818fc2c799e3d5?Expires=1714953600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=CJmnMN~xeNaFztTd-o5LmJDV4Z8rQkVhhLvHO9qoAqfNOc1V7N6DhiGjJYBladto6Z2aSuOEKn3fC1~Pho5rdTidW2Kq6t4M4SoYL3MoMX9lZ7ocLSUJ4qJaOsXe~tGYUTVSGUFMwBR0V0DAP4vhXVwUenP67Ox-13jVE60YaRV0usEJGqUn4CA7Z2Ns0uBEIBmgRtxgvzT1CYQlZDp6yPcGURIbr7PikwhTz21SitAav0eKfQ5ARZgUhiscP7yyUfar8gN47lar3z--MpSmnNpZwZ-4ZdSBet4m7UwmoYUm8~YCfmN6~genc~I3iVBh9lpe-oNxbW-2ASGjLzSyaA__" alt="" className={cx('body2__content__logo')} />
+            <h1 className={cx('body2__content__title')}>UCPC 2024</h1>
+            <p className={cx('body2__content__content')}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           </div>
+        </div>
+      </div>
+      <div className={cx('page3')}>
+        <div className={cx('body1')}>
+          <div className={cx('body1__content')}>
+            <p>TIN TỨC</p>
+          </div>
+          <div className={cx('body1__line')}></div>
+          <div className={cx('body1__content')}>
+            <p>UCPC</p>
+          </div>
+        </div>
+        <div className={cx('body2')}>
+          <div className={cx('body2__box1')}>
+            <div className={cx('body2__box1__container')}>
+              <p className={cx('body2__box1__container__title')}>Massa tortor nibh nulla condimentum imperdiet scelerisque...</p>
+              <p className={cx('body2__box1__container__time')}>2 Hours Ago</p>
+            </div>
+          </div>
+          <div className={cx('body2__box2')}>
+            <div className={cx('body2__box2__content')}>
+              <div className={cx('body2__box2__content__container')}>
+                <p className={cx('body2__box2__content__container__title')}>News Title Lorem Ipsum Dolor Sit Amet</p>
+                <p className={cx('body2__box2__content__container__time')}>1 Hour Ago</p>
+              </div>
+              <div className={cx('body2__box2__content__img')}></div>
+            </div>
+            <div className={cx('body2__box2__content')}>
+              <div className={cx('body2__box2__content__container')}>
+                <p className={cx('body2__box2__content__container__title')}>News Title Lorem Ipsum Dolor Sit Amet</p>
+                <p className={cx('body2__box2__content__container__time')}>1 Hour Ago</p>
+              </div>
+              <div className={cx('body2__box2__content__img')}></div>
+            </div>
+            <div className={cx('body2__box2__content')}>
+              <div className={cx('body2__box2__content__container')}>
+                <p className={cx('body2__box2__content__container__title')}>News Title Lorem Ipsum Dolor Sit Amet</p>
+                <p className={cx('body2__box2__content__container__time')}>1 Hour Ago</p>
+              </div>
+              <div className={cx('body2__box2__content__img')}></div>
+            </div>
+            <div className={cx('body2__box2__footer')}>
+              <button className={cx('body2__box2__footer__botton')}>Xem thêm</button>
+            </div>
+          </div>
+
+        </div>
+        <div className={cx('body3')}>
+          <div className={cx('body3__line')}></div>
         </div>
       </div>
     </div>
