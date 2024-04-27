263fbcd849b97fc4b0a15bb9f5232e634c2853de
VieVyyy <22521705@uit.edu.vn>
2024-04-27 15:31:11 +0700

    update register-form

--- Fontend/src/components/Popup/Register/Register.js
+++ Fontend/src/components/Popup/Register/Register.js
@@ -5,22 +5,29 @@ const cx = classNames.bind(styles);
 
 function Register() {
     return <h2 className={cx('wrapper')}>
+        <div className = {cx('top')}>
+            <img src = "https://scontent.fsgn1-1.fna.fbcdn.net/v/t39.30808-6/346619759_548498594143332_8876881902320368395_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeEqeUtYxfxt5C9mA7rdAa_Wwdp9UdzWE6fB2n1R3NYTp8wm_jur-Lev0UPjLOTtEG2NbRwT__c44fHi3djVhgaP&_nc_ohc=lKxRbxoesNEQ7kNvgFAYJds&_nc_ht=scontent.fsgn1-1.fna&oh=00_AfAjb8SnBM2L8k7RcteN05aTr9jQNQ8XONH5YgkhHWy7mA&oe=6632871E"
+            width={50}
+            />
+        </div>
 
+        <form className = {cx('register_form')}>
             <h1>ĐĂNG KÍ</h1>
-            <form id = "register_form">
+            <div className = {cx('register_infor')}>
                 <div className = {cx('register_username')}>
                     <input type = "text" name = "username" size = "30" placeholder = "Tên đăng nhập" required />
                 </div>
-                
                 <div className = {cx('register_pass')}>
                     <input type = "password" name = "pass" size = "30" placeholder = "Nhập mật khẩu" required />
-                    <input type = "checkbox" name = "show_pass_icon" className = {cx('showPassCheckbox')} />
-                    <br/>
+                    <input type = "checkbox" name = "show_pass_icon" className = {cx('showPassCheckbox')} /><div className = {cx('icon')}></div>
+
                     <input type = "password" name = "pass" size = "30" placeholder = "Nhập mật khẩu" required />
-                    <input type = "checkbox" name = "show_pass_icon"  className = {cx('showPassCheckbox')} />
+                    <input type = "checkbox" name = "show_pass_icon" className = {cx('showPassCheckbox')} /><div className = {cx('icon')}></div>
+
                 </div>
+            </div>
             <button type = "submit" className = {cx('register_button')}>Đăng kí</button>
-            </form>
+        </form>
 
     </h2>;
 }
