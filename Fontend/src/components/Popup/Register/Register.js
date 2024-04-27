d0e7cc00624b0da88be3da121af58ee637bcd6da
VieVyyy <22521705@uit.edu.vn>
2024-04-27 10:13:49 +0700

    update content of register form

--- Fontend/src/components/Popup/Register/Register.js
+++ Fontend/src/components/Popup/Register/Register.js
@@ -3,8 +3,26 @@ import styles from './Register.module.scss';
 
 const cx = classNames.bind(styles);
 
-function Home() {
-    return <h2 className={cx('wrapper')}>Home page</h2>;
+function Register() {
+    return <h2 className={cx('wrapper')}>
+
+            <h1>ĐĂNG KÍ</h1>
+            <form id = "register_form">
+                <div className = {cx('register_username')}>
+                    <input type = "text" name = "username" size = "30" placeholder = "Tên đăng nhập" required />
+                </div>
+                
+                <div className = {cx('register_pass')}>
+                    <input type = "password" name = "pass" size = "30" placeholder = "Nhập mật khẩu" required />
+                    <input type = "checkbox" name = "show_pass_icon" className = {cx('showPassCheckbox')} />
+                    <br/>
+                    <input type = "password" name = "pass" size = "30" placeholder = "Nhập mật khẩu" required />
+                    <input type = "checkbox" name = "show_pass_icon"  className = {cx('showPassCheckbox')} />
+                </div>
+            <button type = "submit" className = {cx('register_button')}>Đăng kí</button>
+            </form>
+
+    </h2>;
 }
 
-export default Home;
+export default Register;
