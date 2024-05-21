7dc85250eb52f6428adcfc35aefea0f2538c555c
Võ Nguyễn Hoài Nam <vonguyenhoainam123@gmail.com>
2024-05-22 04:11:08 +0700

    update docker

--- Backend/src/configs/dbConnection.js
+++ Backend/src/configs/dbConnection.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     port: +process.env.DB_PORT,
-    dialect: 'postgres'
+    dialect: process.env.DB_DIALECT,
 });
 export const testConnection = async () => {
     try {
--- Backend/src/seeders/20240510124209-admin-seed.js
+++ Backend/src/seeders/20240510124209-admin-seed.js
@@ -15,8 +15,8 @@ module.exports = {
     await queryInterface.bulkInsert('Users', [{
       id: 1,
       email: 'admin@uit.cs',
-      password: '$2a$10$Oel6pQqsY8qmizae68sY6.gpPDfT/aHU2p7dHWeW6kIx.J8voWKX6',
-      username: 'adminSecret',
+      password: '$2a$12$4VMqo9UFsUTyUIrVIIzALOwCena9OftOp6zPwbUUVU4jmAT2P8Zce',
+      username: 'UCPCAdmin',
       role: 'ADMIN',
       createdAt: new Date(),
       updatedAt: new Date()
