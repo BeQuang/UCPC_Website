fd929dabe9e7e927a423149d04c253f4d6b0ae05
xlinh2301 <nxlinh2301@gmail.com>
2024-05-29 21:40:12 +0700

    update

--- Fontend/src/layouts/Header/Header.js
+++ Fontend/src/layouts/Header/Header.js
@@ -1,5 +1,27 @@
+import styles from './Header.module.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
 function Header() {
-    return <div>Header</div>;
+  return (
+    <header className={cx('header')}>
+      <div className={cx('logo')}>
+        <a href="#">UCPC</a>
+      </div>
+      <div className={cx('arrow-line')}></div>
+      <nav className={cx('nav-links')}>
+        <a href="#">Tin tức</a>
+        <a href="#">Thể lệ</a>
+        <a href="#">Giải thưởng</a>
+        <a href="#">Liên hệ</a>
+      </nav>
+      <div className={cx('auth-buttons')}>
+        <button className={cx('register-btn')}>Đăng ký</button>
+        <button className={cx('login-btn')}>Đăng nhập</button>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
