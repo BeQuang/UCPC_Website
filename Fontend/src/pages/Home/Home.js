ff7428039ebd7011ce040898d0c035f3e3cb82b8
xlinh2301 <nxlinh2301@gmail.com>
2024-06-03 10:45:52 +0700

    update pages

--- Fontend/src/pages/Home/Home.js
+++ Fontend/src/pages/Home/Home.js
@@ -2,17 +2,19 @@ import React from 'react';
 import Introduction from '~/components/Introduction';
 import News from '~/components/News/News';
 import Rules from '~/components/Rules/Rules';
+import EventSchedule from '~/components/EventSchedule/EventSchedule';
 import Header from '~/layouts/Header';
 
 function Home() {
-    return (
-        <div>
-            <Header />
-            <Introduction />
-            <News />
-            <Rules />
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <Introduction />
+      <News />
+      <Rules />
+      <EventSchedule />
+    </div>
+  );
 }
 
 export default Home;
