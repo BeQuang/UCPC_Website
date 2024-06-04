23e2ef5d2b503690e7623afffe979df35e4540bc
xlinh2301 <nxlinh2301@gmail.com>
2024-06-04 18:49:42 +0700

    update registration_home_page

--- Fontend/src/pages/Home/Home.js
+++ Fontend/src/pages/Home/Home.js
@@ -3,6 +3,7 @@ import Introduction from '~/components/Introduction';
 import News from '~/components/News/News';
 import Rules from '~/components/Rules/Rules';
 import EventSchedule from '~/components/EventSchedule/EventSchedule';
+import RegistrationPage from '~/components/RegistrationPage/RegistrationPage';
 import Header from '~/layouts/Header';
 
 function Home() {
@@ -13,6 +14,7 @@ function Home() {
       <News />
       <Rules />
       <EventSchedule />
+      <RegistrationPage />
     </div>
   );
 }
