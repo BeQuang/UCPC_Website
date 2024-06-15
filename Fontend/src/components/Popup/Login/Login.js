99414c26dcef82096cf2e719b15a115356bb099e
nguyenvanminh281005 <135933038+nguyenvanminh281005@users.noreply.github.com>
2024-06-15 12:16:26 +0700

    login page

--- Fontend/src/components/Popup/Login/Login.js
+++ Fontend/src/components/Popup/Login/Login.js
@@ -3,56 +3,76 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 
+import './Login.scss';
+import { ModalTitle } from 'react-bootstrap';
+
 function Login() {
   const [show, setShow] = useState(false);
-
+  const nameLogin = ''
+  const handleLogin = () => {setShow(true)}
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+
+  const Params = ({
+    interval = '-1',
+    startDate = '0000-00-00',
+    endDate = '0000-00-00',
+    onSetParams = f => f,
+    }) =>
+    {
+    let _interval = interval,
+    _startDate = startDate,
+    _endDate = endDate
+    
+    const submit = e => {
+    e.preventDefault()
+  }}
 
   return (
     <>
     
-      <Button variant="primary" onClick={handleShow} className='login'>
+      <Button variant="primary" onClick={handleShow} className='login-button'>
         Đăng nhập
       </Button>
 
-      <Modal show={show} onHide={handleClose}>
+      <Modal show={show} onHide={handleClose} className='popup-login'>
         <Modal.Header closeButton>
-          <Modal.Title>Đăng nhập</Modal.Title>
+          <Modal.Title className='pp1'>CHÀO MỪNG</Modal.Title>
+          <Modal.Title className='pp2'>BẠN ĐẾN VỚI</Modal.Title>
+          <Modal.Title className='pp3'>UCPC</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
+          <Form className='form-login'>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Tên Đăng Nhập, Email</Form.Label>
               <Form.Control
                 type="text"
-                placeholder="nguyenvanminh"
+                placeholder="Tên Đăng Nhập"
                 autoFocus
               />
             </Form.Group>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Mật Khẩu</Form.Label>
               <Form.Control
                 type="text"
                 placeholder="Mật Khẩu"
                 autoFocus
               />
             </Form.Group>
+            <Button variant="primary" onClick={handleLogin}>
+            Đăng Nhập
+          </Button>
           </Form>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant='primary' onClick={handleClose}>
-            Đăng kí 
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Đăng Nhập
-          </Button>
-          <Button variant="secondary" onClick={handleClose}>
-            Đóng
-          </Button>
-          {/* <Button variant="secondary" onClick={handleClose}>
-            Quên Mặt Khẩu
-          </Button> */}
+          <>
+          <label>
+              <input type="checkbox" className='checkbox' />
+              Quên Mật Khẩu
+          </label>
+          <label>
+          <a href='https://www.facebook.com/' className='linkst' >Bạn chưa có tài khoản</a>
+          </label>
+          </>
         </Modal.Footer>
       </Modal>
     </>
