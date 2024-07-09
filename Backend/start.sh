110a354211ebb16202e06e5e08b636bffd52a363
Võ Nguyễn Hoài Nam <vonguyenhoainam123@gmail.com>
2024-07-10 00:10:20 +0700

    add config, readme for dev

--- Backend/start.sh
+++ Backend/start.sh
@@ -2,9 +2,9 @@
 echo "Starting script..."
 if [ ! -f /app/initialized ]; then
   echo "Running migrations and seeds..."
-  until npx sequelize-cli db:migrate:undo:all && npx sequelize db:migrate && npx sequelize db:seed:all
+  until npx sequelize-cli db:migrate:undo:all && npx sequelize-cli db:migrate && npx sequelize-cli db:seed:all
   do
-    echo "Migration failed, retrying in 1 secs..."
+    echo "Migration failed, retrying in 1 sec..."
     sleep 1
   done
   touch /app/initialized
