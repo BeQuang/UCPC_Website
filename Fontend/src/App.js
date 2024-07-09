a489af8a8250b3d7551c501e08b757e59bdda20e
Tam Anh Nguyen Duy <nguyenduytamanh03012004@gmail.com>
2024-07-09 17:04:13 +0700

    Fix error layout UserForm

--- Fontend/src/App.js
+++ Fontend/src/App.js
@@ -4,14 +4,16 @@ import Home from './pages/Home';
 import User from './pages/User';
 import Email from './pages/Email';
 import Admin from './pages/Admin';
+import UserForm from './components/Popup/UserForm';
 
 function App() {
     return (
         <Routes>
-            <Route path="/" element={<Home />} />
+            {/* <Route path="/" element={<Home />} />
             <Route path="/user" element={<User />} />
             <Route path="/email" element={<Email />} />
-            <Route path="/admin" element={<Admin />}></Route>
+            <Route path="/admin" element={<Admin />}></Route> */}
+            <Route path='/' element={<UserForm />} />
         </Routes>
     );
 }
--- Fontend/src/components/Popup/UserForm/UserForm.js
+++ Fontend/src/components/Popup/UserForm/UserForm.js
@@ -19,10 +19,12 @@ const validationSchema = Yup.object({
   phonenumber3: Yup.string().matches(/^[0-9]{10,11}$/, 'Số điện thoại không hợp lệ.').required('Số điện thoại là bắt buộc.'),
   school3: Yup.string().required('Tên trường là bắt buộc.'),
 });
-const NumberOfUser = [1, 2, 3]
+
+const NumberOfUser = [1, 2, 3];
+
 const UserForm = () => {
   return (
-    <div className='wrapper-form-user'> {/* Thẻ div bọc bên ngoài cùng với class CSS */}
+    <div className='wrapper-form-user'>
       <Formik
         initialValues={{
           nameofteam: '',
@@ -40,87 +42,99 @@ const UserForm = () => {
           phonenumber3: '',
           school3: '',
         }}
-        validationSchema={validationSchema} // Gán schema xác thực vào Formik
+        validationSchema={validationSchema}
         onSubmit={(values) => {
-          console.log('Form submitted', values); // Hàm xử lý khi form được submit
+          console.log('Form submitted', values);
         }}
       >
         {({ isSubmitting, touched, errors }) => (
-          <Form className='form-user-container'> {/* Thẻ form với class CSS */}
+          <Form className='form-user-container'>
             <div className='title-form-user'>
-              <h1>Đăng kí đội thi UCPC</h1> {/* Tiêu đề của form */}
+              <h1>Đăng kí đội thi UCPC</h1>
             </div>
-            <span className='separator' style={{ height: '5px' }} /> {/* Đường kẻ ngăn cách */}
+            <span className='separator' style={{ height: '5px' }} />
             <div className='content-form-user'>
               <div className='content1-form-user'>
-                <label htmlFor="nameofteam"><b>Tên đội:</b></label>
-                <Field
-                  type='text'
-                  id="nameofteam"
-                  name="nameofteam"
-                  placeholder='Viết hoa chữ cái đầu tiên. Ví dụ: Team01'
-                  className={touched.nameofteam && errors.nameofteam ? 'input-error' : ''} // Thêm class 'input-error' nếu có lỗi
-                />
-                <ErrorMessage name="nameofteam" component="p" className="error" /> {/* Hiển thị thông báo lỗi nếu có */}
+                <div className='field-container'>
+                  <label htmlFor="nameofteam"><b>Tên đội:</b></label>
+                  <Field
+                    type='text'
+                    id="nameofteam"
+                    name="nameofteam"
+                    placeholder='Viết hoa chữ cái đầu tiên. Ví dụ: Team01'
+                    className={touched.nameofteam && errors.nameofteam ? 'input-error' : ''}
+                  />
+                  <ErrorMessage name="nameofteam" component="p" className="error" />
+                </div>
 
-                <label htmlFor='email'><b>Email:</b></label>
-                <Field
-                  type='email'
-                  id='email'
-                  name='email'
-                  placeholder='Ví dụ: abcd@efgh.com'
-                  className={touched.email && errors.email ? 'input-error' : ''} // Thêm class 'input-error' nếu có lỗi
-                />
-                <ErrorMessage name="email" component="p" className="error" /> {/* Hiển thị thông báo lỗi nếu có */}
+                <div className='field-container'>
+                  <label htmlFor='email'><b>Email:</b></label>
+                  <Field
+                    type='email'
+                    id='email'
+                    name='email'
+                    placeholder='Ví dụ: abcd@efgh.com'
+                    className={touched.email && errors.email ? 'input-error' : ''}
+                  />
+                  <ErrorMessage name="email" component="p" className="error" />
+                </div>
               </div>
-              <span className='separator' /> {/* Đường kẻ ngăn cách */}
+              <span className='separator' />
               {NumberOfUser.map((num) => (
                 <React.Fragment key={num}>
                   <div className={`content${num}-form-user`}>
-                    <span><h2>Thành viên {num} {num === '1' && '(Đội trưởng)'}</h2></span>
+                    <span><h2>Thành viên {num} {num === 1 && '(Đội trưởng)'}</h2></span>
 
-                    <label htmlFor={`fullname${num}`}><b>Họ và tên:</b></label>
-                    <Field
-                      type='text'
-                      id={`fullname${num}`}
-                      name={`fullname${num}`}
-                      placeholder='Ví dụ: Nguyễn Văn A'
-                      className={touched[`fullname${num}`] && errors[`fullname${num}`] ? 'input-error' : ''} // Thêm class 'input-error' nếu có lỗi
-                    />
-                    <ErrorMessage name={`fullname${num}`} component="p" className="error" /> {/* Hiển thị thông báo lỗi nếu có */}
+                    <div className='field-container'>
+                      <label htmlFor={`fullname${num}`}><b>Họ và tên:</b></label>
+                      <Field
+                        type='text'
+                        id={`fullname${num}`}
+                        name={`fullname${num}`}
+                        placeholder='Ví dụ: Nguyễn Văn A'
+                        className={touched[`fullname${num}`] && errors[`fullname${num}`] ? 'input-error' : ''}
+                      />
+                      <ErrorMessage name={`fullname${num}`} component="p" className="error" />
+                    </div>
 
-                    <label htmlFor={`identity${num}`}><b>MSSV/CCCD:</b></label>
-                    <Field
-                      id={`identity${num}`}
-                      name={`identity${num}`}
-                      placeholder='MSSV bao gồm chữ hoặc số hoặc chữ và số. Ví dụ: 1234abcd'
-                      className={touched[`identity${num}`] && errors[`identity${num}`] ? 'input-error' : ''} // Thêm class 'input-error' nếu có lỗi
-                    />
-                    <ErrorMessage name={`identity${num}`} component="p" className="error" /> {/* Hiển thị thông báo lỗi nếu có */}
+                    <div className='field-container'>
+                      <label htmlFor={`identity${num}`}><b>MSSV/CCCD:</b></label>
+                      <Field
+                        id={`identity${num}`}
+                        name={`identity${num}`}
+                        placeholder='MSSV bao gồm chữ hoặc số hoặc chữ và số. Ví dụ: 1234abcd'
+                        className={touched[`identity${num}`] && errors[`identity${num}`] ? 'input-error' : ''}
+                      />
+                      <ErrorMessage name={`identity${num}`} component="p" className="error" />
+                    </div>
 
-                    <label htmlFor={`phonenumber${num}`}><b>Số điện thoại:</b></label>
-                    <Field
-                      id={`phonenumber${num}`}
-                      name={`phonenumber${num}`}
-                      placeholder='Số điện thoại có 10 hoặc 11 chữ số. Ví dụ: 0123456789'
-                      className={touched[`phonenumber${num}`] && errors[`phonenumber${num}`] ? 'input-error' : ''} // Thêm class 'input-error' nếu có lỗi
-                    />
-                    <ErrorMessage name={`phonenumber${num}`} component="p" className="error" /> {/* Hiển thị thông báo lỗi nếu có */}
+                    <div className='field-container'>
+                      <label htmlFor={`phonenumber${num}`}><b>Số điện thoại:</b></label>
+                      <Field
+                        id={`phonenumber${num}`}
+                        name={`phonenumber${num}`}
+                        placeholder='Số điện thoại có 10 hoặc 11 chữ số. Ví dụ: 0123456789'
+                        className={touched[`phonenumber${num}`] && errors[`phonenumber${num}`] ? 'input-error' : ''}
+                      />
+                      <ErrorMessage name={`phonenumber${num}`} component="p" className="error" />
+                    </div>
 
-                    <label htmlFor={`school${num}`}><b>Tên trường:</b></label>
-                    <Field
-                      id={`school${num}`}
-                      name={`school${num}`}
-                      placeholder='Ví dụ: Trường Đại học Công nghệ Thông tin'
-                      className={touched[`school${num}`] && errors[`school${num}`] ? 'input-error' : ''} // Thêm class 'input-error' nếu có lỗi
-                    />
-                    <ErrorMessage name={`school${num}`} component="p" className="error" /> {/* Hiển thị thông báo lỗi nếu có */}
+                    <div className='field-container'>
+                      <label htmlFor={`school${num}`}><b>Tên trường:</b></label>
+                      <Field
+                        id={`school${num}`}
+                        name={`school${num}`}
+                        placeholder='Ví dụ: Trường Đại học Công nghệ Thông tin'
+                        className={touched[`school${num}`] && errors[`school${num}`] ? 'input-error' : ''}
+                      />
+                      <ErrorMessage name={`school${num}`} component="p" className="error" />
+                    </div>
                   </div>
-                  <span className='separator' /> {/* Đường kẻ ngăn cách */}
+                  <span className='separator' />
                 </React.Fragment>
               ))}
               <div className='btn-signup'>
-                <button type="submit" disabled={isSubmitting}><h2>Đăng kí đội</h2></button> {/* Nút đăng ký */}
+                <button type="submit" disabled={isSubmitting}><h2>Đăng kí đội</h2></button>
               </div>
             </div>
           </Form>
@@ -131,4 +145,3 @@ const UserForm = () => {
 };
 
 export default UserForm;
-
