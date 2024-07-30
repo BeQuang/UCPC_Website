9b7a34fd16b0e64cffdcf8caeb303c3a8b92b940
BEQUANG2409 <21522507@gm.uit.edu.vn>
2024-07-30 10:51:23 +0700

    Fixed a few minor bugs that occurred when merging code. gitignore file package-lock.json

--- Fontend/src/layouts/Header/Header.js
+++ Fontend/src/layouts/Header/Header.js
@@ -1,5 +1,4 @@
-import styles from './Header.module.scss';
-import classNames from 'classnames/bind';
+/* eslint-disable jsx-a11y/alt-text */
 import { Link } from 'react-scroll';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
@@ -8,119 +7,114 @@ import Popup from 'reactjs-popup';
 import Register from '~/components/Popup/Register';
 import Login from '~/components/Popup/Login';
 import Dropdown from 'react-bootstrap/Dropdown';
-import avt from '~/assets/image/Description_Intro.jpg'
-
-const cx = classNames.bind(styles);
+import avt from '~/assets/image/Description_Intro.jpg';
+import './Header.scss';
 
 function Header() {
-  const [backToTopButton, setBackToTopButton] = useState(false);
-  const [openRegister, setOpenRegister] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [backToTopButton, setBackToTopButton] = useState(false);
+    const [openRegister, setOpenRegister] = useState(false);
+    const [openLogin, setOpenLogin] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 100) {
-        setBackToTopButton(true);
-      } else {
-        setBackToTopButton(false);
-      }
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 100) {
+                setBackToTopButton(true);
+            } else {
+                setBackToTopButton(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+    // Xét trạng thái cho nút đăng ký
+    const handleRegister = () => {
+        setOpenRegister(true);
+    };
+    const onCloseRegister = () => {
+        setOpenRegister(false);
     };
 
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+    const handleLogin = () => {
+        setOpenLogin(true);
+    };
+    const onCloseLogin = () => {
+        setOpenLogin(false);
+    };
 
-  // Xét trạng thái cho nút đăng ký
-  const handleRegister = () => {
-    setOpenRegister(true);
-  };
+    // Xét trạng thái cho nút đăng xuất
+    const handleLogout = () => {
+        setIsLoggedIn(false);
+    };
 
-  const [openRegister, setOpenRegister] = useState(false);
-  const handleRegister = () => {
-      setOpenRegister(true);
-  };
-  const onCloseRegister = () => {
-      setOpenRegister(false);
-  };
+    return (
+        <header className={'header'}>
+            {backToTopButton && (
+                <Link to="video-intro-section" className={'back-text'} smooth={true} duration={500} offset={-70}>
+                    <FontAwesomeIcon icon={faArrowUp} className={'back-icon'} />
+                </Link>
+            )}
+            <div className={'logo'}>
+                <Link to="video-intro-section" smooth={true} duration={500} offset={-70}>
+                    UCPC
+                </Link>
+            </div>
+            <div className={'arrow-line'}></div>
+            <nav className={'nav-links'}>
+                <Link to="news-section" smooth={true} duration={500}>
+                    Tin tức
+                </Link>
+                <Link to="rules-section" smooth={true} duration={500}>
+                    Thể lệ
+                </Link>
+                <Link to="registration-section" smooth={true} duration={500} offset={-70}>
+                    Giải thưởng
+                </Link>
+                <Link to="homefooter-section" smooth={true} duration={500}>
+                    Liên hệ
+                </Link>
+            </nav>
+            <div className={'auth-buttons'}>
+                {isLoggedIn ? (
+                    <>
+                        <Dropdown>
+                            <Dropdown.Toggle variant="dark" bg="dark" id="dropdown-basic" className={'avatar-toggle'}>
+                                <img src={avt} className={'avatar-image'} />
+                            </Dropdown.Toggle>
 
-  const [openLogin, setOpenLogin] = useState(false);
-  const handleLogin = () => {
-    setOpenLogin(true);
-  };
-  const onCloseLogin = () => {
-    setOpenLogin(false);
-  };
-
-  // Xét trạng thái cho nút đăng xuất
-  const handleLogout = () => {
-    setIsLoggedIn(false);
-  }
-
-
-  return (
-    <header className={cx('header')}>
-      {backToTopButton && (
-        <Link to='video-intro-section' className={cx('back-text')} smooth={true} duration={500} offset={-70}>
-          <FontAwesomeIcon icon={faArrowUp} className={cx('back-icon')} />
-        </Link>
-      )}
-      <div className={cx('logo')}>
-        <Link to='video-intro-section' smooth={true} duration={500} offset={-70}>UCPC</Link>
-      </div>
-      <div className={cx('arrow-line')}></div>
-      <nav className={cx('nav-links')}>
-        <Link to='news-section' smooth={true} duration={500}>Tin tức</Link>
-        <Link to='rules-section' smooth={true} duration={500}>Thể lệ</Link>
-        <Link to='registration-section' smooth={true} duration={500} offset={-70}>Giải thưởng</Link>
-        <Link to='homefooter-section' smooth={true} duration={500}>Liên hệ</Link>
-      </nav>
-      <div className={cx('auth-buttons')}>
-        <button className={cx('register-btn')} onClick={() => handleRegister()}>Đăng ký</button>
-        <Popup open={openRegister} onClose={() => setOpenRegister(false)}>
-          <Register close = {onCloseRegister}/>   
-        </Popup>
-        <button className={cx('login-btn')} onClick={() => handleLogin()}>Đăng nhập</button>
-        <Popup open={openLogin} onClose={() => setOpenLogin(false)}>
-          <Login close = {onCloseLogin}/>   
-        </Popup>
-        {isLoggedIn ? (
-          <>
-            <Dropdown>
-              <Dropdown.Toggle variant="dark" bg="dark" id="dropdown-basic" className={cx('avatar-toggle')}>
-                <img
-                  src={avt}
-                  className={cx('avatar-image')}
-                />
-              </Dropdown.Toggle>
-
-              <Dropdown.Menu>
-                <Dropdown.Item href="#/action-1">
-                  <img
-                    src={avt}
-                    className={cx('avatar-image')}
-                  />
-                  Tên
-                </Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                <Dropdown.Item href="/" onClick={handleLogout}>Đăng xuất</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          </>
-        ) : (
-          <>
-            <button className={cx('register-btn')} onClick={handleRegister}>Đăng ký</button>
-            <button className={cx('login-btn')}>Đăng nhập</button>
-          </>
-        )}
-      </div>
-      <Popup
-        open={openRegister}
-        onClose={() => setOpenRegister(false)}
-      >
-        <Register />
-      </Popup>
-    </header>
-  );
+                            <Dropdown.Menu>
+                                <Dropdown.Item href="#/action-1">
+                                    <img src={avt} className={'avatar-image'} />
+                                    Tên
+                                </Dropdown.Item>
+                                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                                <Dropdown.Item href="/" onClick={handleLogout}>
+                                    Đăng xuất
+                                </Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </>
+                ) : (
+                    <>
+                        <button className={'register-btn'} onClick={handleRegister}>
+                            Đăng ký
+                        </button>
+                        <button className={'login-btn'} onClick={() => handleLogin()}>
+                            Đăng nhập
+                        </button>
+                    </>
+                )}
+            </div>
+            <Popup open={openRegister} onClose={() => setOpenRegister(false)}>
+                <Register close={onCloseRegister} />
+            </Popup>
+            <Popup open={openLogin} onClose={() => setOpenLogin(false)}>
+                <Login close={onCloseLogin} />
+            </Popup>
+        </header>
+    );
 }
 
 export default Header;
